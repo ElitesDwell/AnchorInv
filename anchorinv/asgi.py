ff5c33bbb5aeb88ba69b080ca55a7157fa6815e8
affluenceinvestmentcompany <affluenceinvestmentcompany@gmail.com>
2025-03-02 17:42:55 +0100

    done

--- anchorinv/asgi.py
+++ anchorinv/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for affluenceinvestment project.
+ASGI config for anchorinv project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'affluenceinvestment.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'anchorinv.settings')
 
 application = get_asgi_application()
--- anchorinv/wsgi.py
+++ anchorinv/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for affluenceinvestment project.
+WSGI config for anchorinv project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'affluenceinvestment.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'anchorinv.settings')
 
 application = get_wsgi_application()
