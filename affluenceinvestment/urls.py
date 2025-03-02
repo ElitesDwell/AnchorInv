68a2f8f4e1fa389114614661f4ed23b037f4d55b
affluenceinvestmentcompany <affluenceinvestmentcompany@gmail.com>
2025-03-02 17:51:19 +0100

    done

--- affluenceinvestment/urls.py
+++ affluenceinvestment/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for anchorinv project.
+URL configuration for affluenceinvestment project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', include('home.urls')),
+    path('account/', include('account.urls')),
+    path('about/', include('about.urls')),
+    path('privacy-policy/', include('privacyPolicy.urls')),
+    path('terms-conditions/', include('termsConditions.urls')),
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- affluenceinvestment/wsgi.py
+++ affluenceinvestment/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for anchorinv project.
+WSGI config for affluenceinvestment project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'anchorinv.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'affluenceinvestment.settings')
 
 application = get_wsgi_application()
